faeaf1992eb3948783f1f1f6b9f71732775c102e
Husen Efendi <husenef@gmail.com>
2021-08-05 11:08:54 +0700

    update

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, } from "react-router-dom"
 import { Container, makeStyles, } from '@material-ui/core'
 
 import TopHeader from "./Header"
@@ -9,19 +9,25 @@ import Content from './Content'
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: '#F5F7F8',
+    },
+    container: {
+        backgroundColor: '#fff',
+        minHeight: '100%'
     }
 }))
 
 const Layout = ({ children }) => {
     const classes = useStyles()
-    return <Router>
-        <TopHeader />
-        <Container className={classes.root}>
-            <Content />
-            {children !== null && children}
-        </Container>
-        <Footer />
-    </Router>
+    return <Container maxWidth="sm" className={classes.container}>
+        <Router>
+            <TopHeader />
+            <section className={classes.root}>
+                <Content />
+                {children !== null && children}
+            </section>
+            <Footer />
+        </Router>
+    </Container>
 }
 
 Layout.defaultProps = {
